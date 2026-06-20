6757c5374d025598673ec9e11b40a12d8a3e6dc3
dainb <dai15081999@gmail.com>
2026-06-20 23:23:27 +0700

    1

--- services/location-service/src/main/java/com/zosh/location_service/controller/HomeController.java
+++ services/location-service/src/main/java/com/zosh/location_service/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.zosh.location_service.controller;
 
+import com.zosh.payload.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+    @GetMapping()
+    public ApiResponse HomeController() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("haha");
+        return apiResponse;
+    }
 }
